99505e799956a56428ffde606de6141b45f833e0
slin <s.lin12@gmail.com>
2017-08-29 01:37:33 +0000

    extract redundacy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+    
+    before_action :set_article, only: [:edit, :update, :show, :destroy]
 
 def index
     @articles = Article.all
@@ -9,7 +11,6 @@ def new
 end
 
 def edit
-    @article = Article.find(params[:id])
 end
 
 def create
@@ -23,7 +24,6 @@ def create
 end
 
 def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
         flash[:notice] = "Article was successfully updated."
         redirect_to article_path(@article)
@@ -33,19 +33,24 @@ def update
 end
 
 def show 
-    @article = Article.find(params[:id])
 end 
 
 def destroy
-    @article = Article.find(params[:id])
+    
     @article.destroy
     redirect_to articles_path
     flash[:notice] = "Article was successfully deleted."
 end
 
+
 private
+
 def article_params
     params.require(:article).permit(:title, :description)
+end
+
+def set_article 
+    @article = Article.find(params[:id])
 end
 
 
