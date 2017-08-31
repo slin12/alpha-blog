2d04e333ab2c8a6d81ef6a685707d74e673987d5
slin <s.lin12@gmail.com>
2017-08-31 00:24:30 +0000

    style errors messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,7 +16,7 @@ end
 def create
     @article = Article.new(article_params)
     if @article.save
-        flash[:notice] = "Article was successfully created."
+        flash[:success] = "Article was successfully created."
         redirect_to article_path(@article)
     else
         render 'new'
@@ -25,7 +25,7 @@ end
 
 def update
     if @article.update(article_params)
-        flash[:notice] = "Article was successfully updated."
+        flash[:success] = "Article was successfully updated."
         redirect_to article_path(@article)
     else
         render 'edit'
@@ -39,7 +39,7 @@ def destroy
     
     @article.destroy
     redirect_to articles_path
-    flash[:notice] = "Article was successfully deleted."
+    flash[:danger] = "Article was successfully deleted."
 end
 
 
