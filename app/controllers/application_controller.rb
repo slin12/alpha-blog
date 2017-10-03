e94b1093a9246202c6cca2fc0cb63dd0e86cce68
slin <s.lin12@gmail.com>
2017-10-03 15:26:35 +0000

    complete authentication system

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,7 @@ class ApplicationController < ActionController::Base
   end
   
   def require_user
-    if !logged_in 
+    if !logged_in?
       flash[:danger] = "You must be logged in"
       redirect_to login_path 
     end
